3653d2ab1aceec49fb4270340235180992d3d4fa
Jon Bergerandi <jonbergerandi@gmail.com>
2020-04-10 10:47:57 +0200

    añadido vimwiki

---
+++ vimwiki/wikiToMd.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/env perl -w
+
+use strict;
+use warnings;
+use feature qw(say);
+
+foreach my $file (@ARGV) {
+    if ( -f $file && $file =~ /wiki$/ ) {
+        ( my $copyFile = $file ) =~ s/wiki$/md/;
+        say "$file -> $copyFile";
+        open( my $f,         '<', $file );
+        open( my $escritura, '>', $copyFile );
+        while (<$f>) {
+            s/^=\s?(.*)\s?=$/# $1/;
+            s/^==\s?(.*)\s?==$/## $1/;
+            s/^===\s?(.*)\s?===$/### $1/;
+            s/^====\s? (.*)\s?====$/#### $1/;
+            s/^=====\s?(.*)\s?=====$/##### $1/;
+            s/^======\s?(.*)\s?======$/###### $1/;
+            s/\{\{\{|\}\}\}/\`\`\`/g;
+            s/\{\{\s*(.*?)\|\s*(.*?)\}\}/![$2]\($1\)/g;
+            s/\{\{\s*(.*)\}\}/![$1]\($1\)/g;
+            s/\[\[\s*(.*?)\|\s*(.*?)\]\]/[$2]\($1\)/g;
+            s/\[\[\s*(.*?)\]\]/[$2]\($1\)/g;
+            s/\*(.*?)\*/\*\*$1\*\*/g;
+            s/_(.*?)_/\*$1\*/g;
+            print $escritura $_;
+        }
+        close($escritura);
+        close($f);
+    }
+    else {
+        say "$file no es archivo valido";
+    }
+
+}
