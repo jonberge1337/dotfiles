824d932dacfbfb7988ea8f1525524b5386bcc410
Jon <jonbergerandi@gmail.com>
2020-04-10 19:30:07 +0200

    añadido sudo

--- arch/5installAux.sh
+++ arch/5installAux.sh
@@ -1,25 +1,25 @@
-pacman -S i3wm --noconfirm --needed
-pacman -S mpv --noconfirm --needed
-pacman -S firefox --noconfirm --needed
-pacman -S qutebrowser --noconfirm --needed
-pacman -S chromium --noconfirm --needed
-pacman -S vlc --noconfirm --needed
-pacman -S scrot --noconfirm --needed
-pacman -S npm --noconfirm --needed
-pacman -S yarn --noconfirm --needed
-pacman -S neovim --noconfirm --needed
-pacman -S i3blocks --noconfirm --needed
-pacman -S clang --noconfirm --needed
-pacman -S mpv --noconfirm --needed
-pacman -S progress --noconfirm --needed
-pacman -S zathura --noconfirm --needed
-pacman -S zathura-pdf-mupdf --noconfirm --needed
-pacman -S udisks2 --noconfirm --needed
-pacman -S sxiv --noconfirm --needed
-pacman -S fd --noconfirm --needed
-pacman -S transmission-gtk --noconfirm --needed
-pacman -S perl-tidy --noconfirm --needed
-pacman -S zsh --noconfirm --needed
+sudo pacman -S i3wm --noconfirm --needed
+sudo pacman -S mpv --noconfirm --needed
+sudo pacman -S firefox --noconfirm --needed
+sudo pacman -S qutebrowser --noconfirm --needed
+sudo pacman -S chromium --noconfirm --needed
+sudo pacman -S vlc --noconfirm --needed
+sudo pacman -S scrot --noconfirm --needed
+sudo pacman -S npm --noconfirm --needed
+sudo pacman -S yarn --noconfirm --needed
+sudo pacman -S neovim --noconfirm --needed
+sudo pacman -S i3blocks --noconfirm --needed
+sudo pacman -S clang --noconfirm --needed
+sudo pacman -S mpv --noconfirm --needed
+sudo pacman -S progress --noconfirm --needed
+sudo pacman -S zathura --noconfirm --needed
+sudo pacman -S zathura-pdf-mupdf --noconfirm --needed
+sudo pacman -S udisks2 --noconfirm --needed
+sudo pacman -S sxiv --noconfirm --needed
+sudo pacman -S fd --noconfirm --needed
+sudo pacman -S transmission-gtk --noconfirm --needed
+sudo pacman -S perl-tidy --noconfirm --needed
+sudo pacman -S zsh --noconfirm --needed
 
 yay -S ruby-neovim --noconfirm
 yay -S nodejs-neovim --noconfirm
