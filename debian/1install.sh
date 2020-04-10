1f76d05c3f1c8b3be2c622e966acb8a4711684b6
Jon <jonbergerandi@gmail.com>
2020-04-10 19:44:01 +0200

    empezando a preparar para debian

---
+++ debian/1install.sh
@@ -0,0 +1,7 @@
+sudo aptitude install mlocate -y
+sudo aptitude install p7zip-full -y
+sudo aptitude install alsa-utils -y
+sudo aptitude install pulseaudio -y
+sudo aptitude install pavucontrol -y
+sudo aptitude install nvidia-driver -y
+sudo aptitude install xorg -y
